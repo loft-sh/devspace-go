6cba68be021cf2cd442baafaa8a5f74782d581c1
Levent Ogut <levent@ogut.me>
2022-03-11 00:42:10 +0300

    initial commit

--- main.go
+++ main.go
@@ -1,27 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 // main function
 func main() {
 	router := gin.Default()
-
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
-	// 4
-	router.GET("/xing", func(c *gin.Context) {
-		c.String(http.StatusOK, "zong")
-	})
-
 	router.GET("/hello/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
 	})
-
 	router.Run(":8080")
 }
